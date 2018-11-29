08e9effb58cb3253707a9aa6861bb5ad58ca5723
skino0 <skinogigo@gmail.com>
2018-11-29 14:48:14 +0200

    Adding UI Left Slide hoverable menu

---
+++ js/hover.js
@@ -0,0 +1,35 @@
+//Do not touch any line on this file <3
+
+
+$("#menubar").hover(
+function () {
+$(".meitemplus").addClass('ho');
+},
+function () {
+$(".meitemplus").removeClass('ho');
+}
+);
+$("#menubar").hover(
+function () {
+$(".menuitem").addClass('hoitem');
+},
+function () {
+$(".menuitem").removeClass('hoitem');
+}
+);
+$("#menubar").hover(
+function () {
+$(".icons").addClass('hoicon');
+},
+function () {
+$(".icons").removeClass('hoicon');
+}
+);
+$("#menubar").hover(
+function () {
+$(".meitemplus").addClass('hoitemplus');
+},
+function () {
+$(".meitemplus").removeClass('hoitemplus');
+}
+);
